787a6cd59f59629a78d7e97632f975d1ef42ad64
Rostislav Wolny <rosta@mailpoet.com>
2021-05-04 17:34:03 +0200

    Add guest customers into woo in country segment
    [MAILPOET-3226]

--- lib/Segments/DynamicSegments/Filters/WooCommerceCountry.php
+++ lib/Segments/DynamicSegments/Filters/WooCommerceCountry.php
@@ -22,18 +22,36 @@ class WooCommerceCountry implements Filter {
     $filterData = $filter->getFilterData();
     $countryCode = (string)$filterData->getParam('country_code');
     $countryFilterParam = 'countryCode' . $filter->getId();
+    $collation = $this->getCollate();
     $subscribersTable = $this->entityManager->getClassMetadata(SubscriberEntity::class)->getTableName();
     return $queryBuilder->innerJoin(
       $subscribersTable,
       $wpdb->postmeta,
       'postmeta',
-      "postmeta.meta_key = '_customer_user'
-        AND $subscribersTable.wp_user_id=postmeta.meta_value
+      "postmeta.meta_key = '_billing_email'
+        AND $subscribersTable.email=postmeta.meta_value $collation
+        AND $subscribersTable.is_woocommerce_user = 1
         AND postmeta.post_id NOT IN ( SELECT id FROM {$wpdb->posts} as p WHERE p.post_status IN ('wc-cancelled', 'wc-failed'))"
     )->innerJoin('postmeta',
       $wpdb->postmeta,
       'postmetaCountry',
       "postmeta.post_id = postmetaCountry.post_id AND postmetaCountry.meta_key = '_billing_country' AND postmetaCountry.meta_value = :$countryFilterParam"
     )->setParameter($countryFilterParam, $countryCode);
+  }
+
+  private function getCollate(): string {
+    global $wpdb;
+    $mailpoetEmailColumn = $wpdb->get_row(
+      'SHOW FULL COLUMNS FROM ' . MP_SUBSCRIBERS_TABLE . ' WHERE Field = "email"'
+    );
+    $mailpoetEmailCollation = $mailpoetEmailColumn->Collation; // phpcs:ignore Squiz.NamingConventions.ValidVariableName.NotCamelCaps
+    $wpPostmetaValueColumn = $wpdb->get_row(
+      'SHOW FULL COLUMNS FROM ' . $wpdb->postmeta . ' WHERE Field = "meta_value"'
+    );
+    $wpPostmetaValueCollation = $wpPostmetaValueColumn->Collation; // phpcs:ignore Squiz.NamingConventions.ValidVariableName.NotCamelCaps
+    if ($mailpoetEmailCollation !== $wpPostmetaValueCollation) {
+      return "COLLATE $mailpoetEmailCollation";
+    }
+    return '';
   }
 }
